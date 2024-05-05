a6bfc6337cca0e78f7159463595774872da2b07b
Ahmetbey-1 <ayazdirahmet@gmail.com>
2024-05-05 13:47:22 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center py-[500px] justify-between bg-lacivert/90 text-white">
-      asd
+      test
     </main>
   );
 }
